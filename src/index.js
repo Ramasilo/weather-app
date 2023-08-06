f3492eb9628e9818771df001449d78ad53c211fb
Ramasilo <preciousramasilo97@gmail.com>
2023-08-06 18:33:45 +0200

    Added the forecast template in JS

--- src/index.js
+++ src/index.js
@@ -21,6 +21,31 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Fri", "Sat", "Sun", "Mon"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png" alt="" width="40">
+        <div class="weather-forecast-temperature">
+          <span class="weather-forecast-temperature-max">18°</span> 
+          <span class="weather-forecast-temperature-min">12°</span>
+         </div>
+       </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -91,3 +116,4 @@ function displayTemperature(response) {
   celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
   search("Johannesburg");
+  displayForecast();
